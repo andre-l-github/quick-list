de63b42636c1649825247ba27fd1318a66743d8d
andre <andre.lahs.mail@googlemail.com>
2014-07-28 17:25:16 +0200

    Add edit list page

--- spec/acceptance/landing_spec.rb
+++ spec/acceptance/landing_spec.rb
@@ -2,10 +2,19 @@ require 'acceptance_helper'
 
 feature "Landing Page" do
 
+  let(:list_name) { "Quick-List Features Current Sprint" }
+
   scenario "shows all todo lists", :js => true do
     visit '/'
     page.should have_content('Welcome to Quick-List')
 
-    page.should have_content('Quick-List Features Current Sprint')
+    page.should have_content(list_name)
+    page.should have_content('Open Items')
+    page.should have_content('Closed Items')
+
+    click_link list_name
+
+    page.should have_content "Edit: #{list_name}"
+    current_url.should =~ /#\/lists\/1/
   end
 end
